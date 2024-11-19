d43cddd54db477cda4ff932b1149c640ffad3786
Kyla Usi <129689808+kylamusi@users.noreply.github.com>
2024-11-18 20:27:37 -0800

    Added Nav Bar, Pages, and Functionality for Create Poll (#14)
    * Added Nav Bar, Pages, and Functionality for Create Poll
    
    * Create Poll Edits

---
+++ app/(tabs)/notifs/index.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import React from 'react'
+
+export default function FeedScreen() {
+    return (
+        <ThemedView style={styles.fullPage}>
+            <SafeAreaView>
+                <ThemedView style={styles.titleContainer}>
+                    <ThemedText type="title">Friend Requests!</ThemedText>
+                </ThemedView>
+                <ThemedView style={styles.body}>
+                    <ThemedText type="subtitle">We will see friend requests here based on the status flag of friendships.</ThemedText>
+                </ThemedView>
+            </SafeAreaView>
+        </ThemedView>
+    );
+}
+
+const styles = StyleSheet.create({
+    fullPage: {
+        flex: 1
+    },
+    content: {
+
+    },
+    titleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+    },
+    body: {
+        gap: 8,
+        marginBottom: 8,
+    },
+    reactLogo: {
+        height: 178,
+        width: 290,
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+    },
+});
